2401bbbf61be6fc22c48b226074b8e0f01a24b33
Abedi3 <bluebedi@gmail.com>
2018-01-29 16:29:13 +0530

    Dashboard and hero components + Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [  AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
-  imports: [  BrowserModule, FormsModule ],
+  declarations: [  AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent ],
+  imports: [  BrowserModule, FormsModule, AppRoutingModule ],
   providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
